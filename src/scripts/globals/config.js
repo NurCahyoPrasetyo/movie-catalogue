ba5aa6f733604c93741a6c27c7ae2d140b477faa
nurcahyoprasetiyo <nurcahyoprasetiyo4@gmail.com>
2024-03-26 09:36:57 +0700

    feat: add request API Movie

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: "d22737e6315b8c8aed94d30c15be44cd",
+  BASE_URL: "https://api.themoviedb.org/3/",
+  BASE_IMAGE_URL: "https://image.tmdb.org/t/p/w500/",
+  DEFAULT_LANGUAGE: "en-us",
+};
+
+export default CONFIG;
