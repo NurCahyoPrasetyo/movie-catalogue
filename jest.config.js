94f9ea89cfd3cdfd29362f86400959185ffe8229
nurcahyoprasetiyo <nurcahyoprasetiyo4@gmail.com>
2024-04-16 16:42:59 +0700

    feat: add env config

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ const config = {
   testMatch: ["**/tests/**/*.test.[jt]s?(x)"],
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFiles: ["fake-indexeddb/auto"],
+  setupFiles: ["fake-indexeddb/auto", "dotenv/config"],
 
   // The test environment that will be used for testing
   testEnvironment: "jsdom",
--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: "d22737e6315b8c8aed94d30c15be44cd",
+  KEY: process.env.API_KEY_TMDB,
   BASE_URL: "https://api.themoviedb.org/3/",
   BASE_IMAGE_URL: "https://image.tmdb.org/t/p/w500/",
   DEFAULT_LANGUAGE: "en-us",
